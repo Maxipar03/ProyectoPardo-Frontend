1ed266df0a3b8c224abdf97eb6c4afc4bb25a5b8
Maxipar03 <maxipar2003@gmail.com>
2023-04-12 21:58:54 -0300

    Merge branch 'main' of https://github.com/Maxipar03/ProyectoPardo-Frontend

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,12 +3,12 @@ import React from "react"
 const Footer = () => {
     return (
         <footer>
-        <div className="footer">
+            <div className="footer">
                 <p className="footer-contacto">Contacto</p>
                 <p> 4442-3171<i className="fa-solid fa-phone"></i></p>
                 <p> javierpardo08@gmail.com<i className="fa-solid fa-envelope"></i></p>
                 <p> Callao 1045, Villa Madero, Provincia de Buenos Aires<i className="fa-solid fa-map-pin"></i></p>
-        </div>
+            </div>
         </footer>
     )
 }
