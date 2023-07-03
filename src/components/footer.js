43e15010beb6e6b8035dfc777626c51f4189bca1
Maxipar03 <maxipar2003@gmail.com>
2023-07-02 22:04:42 -0300

    Añadiendo funcionalidades y modificando la interfaz

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,13 @@
 import React from "react"
-import logo from "../Logo.png"
 
 const Footer = () => {
     return (
         <footer>
             <div className="footer">
-                <div className="footer-content">
                 <p className="footer-contacto">Contacto</p>
                 <p> 4442-3171<i className="fa-solid fa-phone"></i></p>
                 <p> javierpardo08@gmail.com<i className="fa-solid fa-envelope"></i></p>
                 <p> Callao 1045, Villa Madero, Provincia de Buenos Aires<i className="fa-solid fa-map-pin"></i></p>
-                </div> 
-                <img className="logo-img-footer" src={logo}/>
             </div>
         </footer>
     )
